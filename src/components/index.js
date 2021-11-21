9e145b264fb5c821f0f64cd7b51094ac2541bb1e
Nikhil Nawgiri <the@realest.ninja>
2021-11-21 23:23:15 +0100

    Create basic textarea

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const NoCringeEditor= () => (
-	<span>NoCringeEditor</span>
-);
+import { Editor, EditorState } from "draft-js";
+import { stateToHTML } from "draft-js-export-html";
+
+const NoCringeEditor = () => {
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const currentContent = editorState.getCurrentContent();
+
+  const html = stateToHTML(currentContent);
+  console.log("html:", html);
+
+  return (
+    <div>
+      <hr />
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+      />
+      <hr />
+    </div>
+  );
+};
 
 NoCringeEditor.propTypes = {
 };
