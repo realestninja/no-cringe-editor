ba3db00b91c5e50fe2945dcf4139cc1e34d8f183
Nikhil Nawgiri <the@realest.ninja>
2021-11-21 23:15:00 +0100

    Create NoCringeEditor component

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NoCringeEditor= () => (
+	<span>NoCringeEditor</span>
+);
+
+NoCringeEditor.propTypes = {
+};
+
+NoCringeEditor.defaultProps = {
+};
+
+export default NoCringeEditor;
